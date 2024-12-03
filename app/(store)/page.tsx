db57e712f483f3fe334947979cefd8c3fb6f7bdb
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2024-12-03 13:07:14 +0530

    Create a Helper Function to Get All Products with GROQ Query

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+
+  // console.log(
+  //   crypto.randomUUID().slice(0, 5) +
+  //   `>>> Rerendered the home page cache with ${products.length} products and ${getEnabledCategories.length} categories`
+  // );
+
   return (
     <div>
       <h1>Hello World!</h1>
+
+      {/* render the all products */}
       <Button>Login</Button>
     </div>
   );
