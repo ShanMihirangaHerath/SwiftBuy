a3421ab446714b82ab7ffaa56b435406f614e679
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2024-12-04 12:43:36 +0530

    Implement Framer Motion

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,10 +1,24 @@
 import { Product } from "@/sanity.types";
+import ProductThumbnail from "./ProductThumbnail";
+import { AnimatePresence, motion } from "framer-motion";
 
 function ProductGrid({ products }: { products: Product[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
       {products?.map((product) => {
-        return <ProductThumbnail key={product._id} product={product} />;
+        return (
+          <AnimatePresence key={product._id}>
+            <motion.div
+              layout
+              initial={{ opacity: 0.2 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="flex justify-center"
+            >
+              <ProductThumbnail key={product._id} product={product} />
+            </motion.div>
+          </AnimatePresence>
+        );
       })}
     </div>
   );
