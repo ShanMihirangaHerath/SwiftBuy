2916e6b182e25bf940e33b6df56c6c616f6476aa
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2024-12-07 14:29:08 +0530

    Create the Category Selector

--- components/ProductsView.tsx
+++ components/ProductsView.tsx
@@ -1,5 +1,6 @@
 import { Category, Product } from "@/sanity.types";
 import ProductGrid from "./ProductGrid";
+import { CategorySelectorComponent } from "./ui/category-selector";
 
 interface ProductsViewProps {
   products: Product[];
@@ -10,7 +11,7 @@ const ProductsView = ({ products, categories }: ProductsViewProps) => {
   return <div className="flex flex-col">
     {/* categories */}
     <div className="w-full sm:w-[200px]">
-        {/* <CategorySelectorComponent categories={categories} /> */}
+        <CategorySelectorComponent categories={categories} />
     </div>
 
     {/* products */}
---
+++ components/ui/category-selector.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { ChevronsUpDown, Check } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Category } from "@/sanity.types";
+
+interface CategorySelectorProps {
+  categories: Category[];
+}
+
+export function CategorySelectorComponent({
+  categories,
+}: CategorySelectorProps) {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<string>("");
+  const router = useRouter();
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full max-w-full relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-yellow-500 hover:bg-yellow-700 hover:text-white text-black font-bold py-2 px-4 rounded"
+        >
+          {value
+            ? categories.find((category) => category._id === value)?.title
+            : "Filter by category"}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full p-0">
+        <Command>
+          <CommandInput
+            placeholder="Search category..."
+            className="h-9"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                const selectCategory = categories.find((c) =>
+                  c.title
+                    ?.toLowerCase()
+                    .includes(e.currentTarget.value.toLowerCase())
+                );
+                if (selectCategory?.slug?.current) {
+                  setValue(selectCategory._id);
+                  router.push(`/categories/${selectCategory.slug.current}`);
+                  setOpen(false);
+                }
+              }
+            }}
+          />
+          <CommandList>
+            <CommandEmpty>No Category Found!</CommandEmpty>
+            <CommandGroup>
+              {categories.map((category) => (
+                <CommandItem
+                  key={category._id}
+                  value={category.title}
+                  onSelect={() => {
+                    setValue(value === category._id ? "" : category._id);
+                    router.push(`/categories/${category.slug?.current}`);
+                  }}
+                >
+                  {category.title}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      value === category._id ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
